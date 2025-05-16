15e469cd2c2408e60d801906df353264bccbc04d
HuascarFedor <huascar.fedor@gmail.com>
2025-05-16 17:35:37 -0400

    ejemplo autenticacion concluido

--- testapp/login.php
+++ testapp/login.php
@@ -2,6 +2,26 @@
 session_start();
 require 'db.php';
 
+// Inicializacion conteo de intentos
+if(!isset($_SESSION['loggin_attempts'])) {
+    $_SESSION['loggin_attempts'] = 0;
+    $_SESSION['last_attempt_time'] = time();
+}
+
+// Definimos cantidad de intentos y penalizacion
+$max_attempts = 5;
+$lockout_time = 300;
+
+// Verificamos cantidad de intentos
+if($_SESSION['loggin_attempts'] >= $max_attempts) {
+    $remaining = $lockout_time - (time() - $_SESSION['last_attempt_time']);
+    if($remaining > 0) {
+        die("Acceso bloqueado. Intente nuevamente en ". ceil($remaining/60) ." minutos");
+    }else{
+        $_SESSION['loggin_attempts'] = 0;
+    }
+}
+
 // Generar token CSRF si no existe
 if(empty($_SESSION['csrf_token'])){
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
@@ -26,11 +46,15 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
         exit;
     }
     else{
+        $_SESSION['loggin_attempts'] += 1;
+        $_SESSION['last_attempt_time'] = time();
         echo "Usuario o contraseña incorrectos";
     }
 }
 ?>
 <form method="POST">
+<label>Número de intentos: <?php echo $_SESSION['loggin_attempts']; ?></label>
+<br>
 <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
 Usuario: <input type="text" name="username" required><br>
 contraseña: <input type="password" name="password" required>
